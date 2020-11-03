70f3b0c844c851245dc4d63386d3e8c6d12e78a7
stevenhu0124 <cloudshao1988@gmail.com>
2020-11-03 21:52:56 +0800

    Initial commit

---
+++ Tesseract-OCR/Controller/CaputreScreen.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Tesseract;
+
+
+namespace Tesseract_OCR.Controller
+{
+    public class CaputreScreen
+    {
+        private readonly Rectangle captureScreen;
+        private readonly Rectangle caputreRange;
+
+        public CaputreScreen(int screenWidth, int screenHeight)
+        {
+            captureScreen = new Rectangle() { Width = screenWidth, Height = screenHeight };
+            caputreRange = new Rectangle(new Point(screenWidth / 4, screenHeight / 4), new Size(screenWidth / 2, screenHeight / 2));
+        }
+
+        public string ScanCoordinate()
+        {
+            using Bitmap fullScreen = new Bitmap(captureScreen.Width, captureScreen.Height, PixelFormat.Format32bppArgb);
+
+            using (Graphics graphics = Graphics.FromImage(fullScreen))
+            {
+                graphics.CopyFromScreen(0, 0, 0, 0, captureScreen.Size);
+            }
+
+            MemoryStream ms = new MemoryStream();
+            using Bitmap captureImage = CaptureImage(fullScreen, caputreRange);
+
+            captureImage.Save(AppDomain.CurrentDomain.BaseDirectory + "Test.bmp");
+            captureImage.Save(ms, System.Drawing.Imaging.ImageFormat.Tiff);
+
+
+            return Convert(ms.ToArray());
+        }
+
+        private Bitmap CaptureImage(Bitmap source, Rectangle section)
+        {
+            var bitmap = new Bitmap(section.Width, section.Height, PixelFormat.Format32bppArgb);
+            using var graphics = Graphics.FromImage(bitmap);
+            graphics.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+            return bitmap;
+        }
+
+        private string Convert(byte[] imageBytes)
+        {
+            try
+            {
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadTiffFromMemory(imageBytes))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+                            using (var iter = page.GetIterator())
+                            {
+                                iter.Begin();
+                            }
+                            return text;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Format("{0}, {1},", "Unexpected Error: " + ex.Message, ex.ToString());
+            }
+        }
+    }
+}
---
+++ Tesseract-OCR/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Tesseract_OCR.Controller;
+
+namespace Tesseract_OCR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private readonly CaputreScreen capture = new CaputreScreen(1920, 1080);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _captureText;
+
+        /// <summary>
+        /// the degree of the current wheel color selected.
+        /// </summary>
+        public string CaputreText
+        {
+            get
+            {
+                return _captureText;
+            }
+            set
+            {
+                if (_captureText != value)
+                {
+                    _captureText = value;
+                    NotifyPropertyChanged(nameof(CaputreText));
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = this;
+        }
+
+        private void StartDetect(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    CaputreText = capture.ScanCoordinate();
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+    }
+}
